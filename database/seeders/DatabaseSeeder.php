e1a9d9ae64f220eec424862f18c26b43083f049e
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-01-12 01:25:48 +0200

    adding new seed logic for study and courses

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\City;
+use App\Models\County;
+use App\Models\ProfileCourse;
+use App\Models\ProfileStudy;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -18,8 +22,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Create an admin
-        User::factory(['email' => 'admin@example.com'])
+        $user = User::factory(['email' => 'admin@example.com'])
             ->withProfile()
+            ->create();
+        $county = County::factory(1)->create();
+        $city = City::factory(1)->create();
+
+        ProfileStudy::factory(['user_id' => 1, 'county_id' => 1, 'city_id' => 1])
+//            ->forUser()
+//            ->forCounty()
+//            ->forCity()
+            ->count(3)
+            ->create();
+
+        ProfileCourse::factory(['user_id' => 1])
+            ->count(10)
             ->create();
     }
 }
