1e44305ac9c0effc8befc07b3dfd93f797b511ba
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-01-12 01:22:28 +0200

    Add courses model with migration and factory

---
+++ database/migrations/2023_01_11_210411_create_profile_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Enums\CourseType;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile_courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->year('year');
+            $table->string('provider');
+            $table->string('name');
+            $table->enum('type', CourseType::values());
+            $table->integer('credits');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile_courses');
+    }
+};
