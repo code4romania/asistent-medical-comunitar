0f0ee8c90056a0ebd0327777443aff0fad941ee4
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-01-12 01:23:24 +0200

    Adding dummy models for county and city for testing purpose

---
+++ database/factories/CityFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\City;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<City>
+ */
+class CityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'=> fake()->city(),
+        ];
+    }
+}
---
+++ database/factories/CountyFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\County;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<County>
+ */
+class CountyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'=>fake()->city()
+        ];
+    }
+}
