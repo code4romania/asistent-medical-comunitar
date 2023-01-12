dd9cde5bf35cf55401e904c7b9e7d3a831601409
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-01-12 14:11:59 +0200

    Refactoring seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Area;
 use App\Models\City;
 use App\Models\County;
 use App\Models\ProfileCourse;
@@ -24,26 +25,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Create an admin
-        $user = User::factory(['email' => 'admin@example.com'])
+        User::factory(['email' => 'admin@example.com'])
             ->withProfile()
             ->create();
-        $county = County::factory(1)->create();
-        $city = City::factory(1)->create();
-
-        ProfileStudy::factory(['user_id' => 1, 'county_id' => 1, 'city_id' => 1])
-//            ->forUser()
-//            ->forCounty()
-//            ->forCity()
-            ->count(3)
-            ->create();
-
-        ProfileCourse::factory(['user_id' => 1])
-            ->count(10)
-            ->create();
-
-        ProfileEmployer::factory(['user_id' => 1, 'county_id' => 1, 'city_id' => 1, 'end_date'=>now()])->count(3)->create();
-
-        ProfileEmployer::factory(['user_id' => 1, 'county_id' => 1, 'city_id' => 1])->count(3)->create();
-        ProfileEmployer::factory(['user_id' => 1, 'county_id' => 1, 'city_id' => 1, 'project_name'=>'Proiect '. Str::createRandomStringsNormally()])->count(3)->create();
+        County::factory(1)->create();
+        City::factory(1)->create();
+        $this->run([ProfileSeeder::class]);
     }
 }
