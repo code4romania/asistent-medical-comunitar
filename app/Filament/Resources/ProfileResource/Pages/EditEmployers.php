3e507f71b2513338a8d4d07beb4dc1f353686a17
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-01-23 16:17:12 +0200

    add virtual checkbox

--- app/Filament/Resources/ProfileResource/Pages/EditEmployers.php
+++ app/Filament/Resources/ProfileResource/Pages/EditEmployers.php
@@ -7,7 +7,7 @@ namespace App\Filament\Resources\ProfileResource\Pages;
 use App\Enums\EmployerType;
 use App\Forms\Components\Location;
 use App\Forms\Components\Subsection;
-use App\Models\Profile\Employer;
+use Closure;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Repeater;
@@ -36,16 +36,33 @@ class EditEmployers extends EditRecord
                                     ->label(__('user.profile.field.employer.type'))
                                     ->options(EmployerType::options())
                                     ->required(),
-                                Checkbox::make('project_base'),
-//                                    ->hidden(fn(Employer $record) => empty($record->project_name)),
+                                Checkbox::make('is_project_base')
+                                    ->label(__('user.profile.field.employer.project_base'))
+                                    ->reactive(),
                                 TextInput::make('project')
-                                    ->label(__('user.profile.field.employer.project')),
+                                    ->label(__('user.profile.field.employer.project'))
+                                    ->hidden(function (callable $get) {
+                                        return $get('is_project_base') === false;
+                                    })
+                                    ->reactive()
+                                    ->afterStateHydrated(function (Closure $set, $state) {
+                                        $set('is_project_base', $state !== null);
+                                    }),
                                 Location::make(),
                                 DatePicker::make('start_date')
                                     ->label(__('user.profile.field.start_date')),
                                 DatePicker::make('end_date')
+                                    ->label(__('user.profile.field.end_date'))
                                     ->afterOrEqual('start_date')
-                                    ->label(__('user.profile.field.end_date')),
+                                    ->disabled(function (callable $get) {
+                                        return $get('is_on_going') === true;
+                                    })
+                                    ->afterStateHydrated(function (Closure $set, $state) {
+                                        $set('is_on_going', $state === null);
+                                    }),
+                                Checkbox::make('is_on_going')
+                                    ->label(__('user.profile.field.employer.on_going'))
+                                    ->reactive(),
                             ]),
                     ]),
             ]);
