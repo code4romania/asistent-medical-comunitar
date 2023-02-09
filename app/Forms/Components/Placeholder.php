d65b54cd7e4762c75ebdcf3e8b9a13cec04c2308
Andrei Ioniță <hi@andrei.io>
2023-02-09 16:56:11 +0000

    expand placeholder component

--- app/Forms/Components/Placeholder.php
+++ app/Forms/Components/Placeholder.php
@@ -10,8 +10,35 @@ use Illuminate\Support\Str;
 
 class Placeholder extends BasePlaceholder
 {
+    protected bool $withoutContent = false;
+
+    protected $fallback = null;
+
+    public function withoutContent(): static
+    {
+        $this->withoutContent = true;
+
+        return $this;
+    }
+
+    public function fallback($fallback): static
+    {
+        $this->fallback = $fallback;
+
+        return $this;
+    }
+
+    public function getFallback()
+    {
+        return $this->evaluate($this->fallback);
+    }
+
     public function getContent()
     {
+        if ($this->withoutContent) {
+            return null;
+        }
+
         $content = parent::getContent();
 
         if (! $content instanceof HtmlString) {
@@ -21,7 +48,7 @@ class Placeholder extends BasePlaceholder
         }
 
         if ($content->isEmpty()) {
-            return new HtmlString('<span class="text-gray-500">&mdash;</span>');
+            return $this->getFallback() ?? new HtmlString('<span class="text-gray-500">&mdash;</span>');
         }
 
         return $content;
