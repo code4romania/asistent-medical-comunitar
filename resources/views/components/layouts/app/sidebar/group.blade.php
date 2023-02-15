a3d71ef3ed1e23ab6a54f52b9cea52d0f7986568
Andrei Ioniță <hi@andrei.io>
2023-02-15 15:40:07 +0000

    fix: sidebar layout group

--- resources/views/components/layouts/app/sidebar/group.blade.php
+++ resources/views/components/layouts/app/sidebar/group.blade.php
@@ -1,22 +1,17 @@
 @props([
     'parentGroup' => null,
-    'collapsible' => true,
     'icon' => null,
     'items' => [],
     'label' => null,
 ])
 
-<li
-    x-data="{ label: {{ \Illuminate\Support\Js::from((filled($parentGroup) ? "{$parentGroup}." : null) . $label) }} }"
-    class="filament-sidebar-group"
-    @if (filled($parentGroup)) x-bind:class="{{ config('filament.layout.sidebar.is_collapsible_on_desktop') ? '$store.sidebar.isOpen' : 'true' }} ? 'ml-11 pr-3 pt-3' : 'hidden'" @endif
->
+<li @class([
+    'filament-sidebar-group',
+    'ml-11 pr-3 pt-3' => filled($parentGroup),
+])>
+
     @if ($label)
-        <button
-            @if ($collapsible) x-on:click.prevent="$store.sidebar.toggleCollapsedGroup(label)" @endif
-            @if (config('filament.layout.sidebar.is_collapsible_on_desktop')) x-show="$store.sidebar.isOpen" @endif
-            class="flex items-center justify-between w-full"
-        >
+        <button class="flex items-center justify-between w-full">
             <div @class([
                 'flex items-center gap-4 text-gray-600',
                 'dark:text-gray-300' => config('filament.dark_mode'),
@@ -32,25 +27,10 @@
                     {{ $label }}
                 </p>
             </div>
-
-            @if ($collapsible)
-                <x-heroicon-o-chevron-down
-                    :class="\Illuminate\Support\Arr::toCssClasses([
-                        'w-3 h-3 text-gray-600 transition',
-                        'dark:text-gray-300' => config('filament.dark_mode'),
-                    ])"
-                    x-bind:class="$store.sidebar.groupIsCollapsed(label) || '-rotate-180'"
-                    x-cloak
-                />
-            @endif
         </button>
     @endif
 
-    <ul
-        x-show="! ($store.sidebar.groupIsCollapsed(label) && {{ config('filament.layout.sidebar.is_collapsible_on_desktop') ? '$store.sidebar.isOpen' : 'true' }})"
-        x-collapse.duration.200ms
-        @class(['text-sm space-y-1 -mx-3', 'mt-2' => $label])
-    >
+    <ul @class(['text-sm space-y-1 -mx-3', 'mt-2' => $label])>
         @foreach ($items as $item)
             @if ($item instanceof \Filament\Navigation\NavigationItem)
                 <x-layouts.app.sidebar.item
@@ -68,7 +48,6 @@
                 <x-layouts.app.sidebar.group
                     :label="$item->getLabel()"
                     :icon="$item->getIcon()"
-                    :collapsible="$item->isCollapsible()"
                     :items="$item->getItems()"
                     :parentGroup="(filled($parentGroup) ? '$parentGroup' . '.' : null) . $label"
                 />
