ab6ebc9ca1ff77a117b9fb83d076438db2dab7ee
Andrei Ioniță <hi@andrei.io>
2023-04-20 12:53:09 +0100

    wip

---
+++ app/Filament/Pages/Auth/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Http\Livewire\Auth\Login as BaseLogin;
+
+class Login extends BaseLogin
+{
+    public function mount(): void
+    {
+        parent::mount();
+
+        if (! app()->isLocal()) {
+            return;
+        }
+
+        $this->loginAsNurse();
+    }
+
+    private function loginAsNurse(): void
+    {
+        $this->form->fill([
+            'email' => 'nurse@example.com',
+            'password' => 'password',
+            'remember' => true,
+        ]);
+    }
+}
