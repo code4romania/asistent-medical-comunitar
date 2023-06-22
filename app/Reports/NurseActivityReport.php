06f573f7b55cad5581a680f50e6037d006da1bd0
Andrei Ioniță <hi@andrei.io>
2023-06-22 15:51:14 +0100

    wip

--- app/Reports/NurseActivityReport.php
+++ app/Reports/NurseActivityReport.php
@@ -9,6 +9,7 @@ use App\Models\Activity;
 use App\Models\Beneficiary;
 use Illuminate\Contracts\Database\Query\Expression as ExpressionContract;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Query\JoinClause;
 use Tpetry\QueryExpressions\Language\Alias;
@@ -95,51 +96,46 @@ class NurseActivityReport extends ReportFactory
                     auth()->user()->isNurse(),
                     fn (Builder $query) => $query->where('nurse_id', auth()->id())
                 )
-                ->fromSub(
-                    function ($query) {
-                        $query
-                            ->select(['activity_log.*', 'beneficiaries.nurse_id', 'beneficiaries.gender'])
-                            ->selectRaw('TIMESTAMPDIFF(YEAR, date_of_birth, ?) AS age', [$this->report->date_from->toDateString()])
-                            ->from('activity_log')
-                            ->leftJoin('beneficiaries', function (JoinClause $join) {
-                                $join->on('beneficiaries.id', '=', 'activity_log.subject_id')
-                                    ->where('activity_log.subject_type', 'beneficiary');
-                            });
-                    },
-                    'activity_log'
-                )
-                ->when(
-                    $indicators->isNotEmpty(),
-                    function (Builder $query) use ($indicators) {
-                        $cases = $indicators
-                            ->mapWithKeys(fn (string $indicator) => [
-                                $indicator => match ($indicator) {
-                                    'adult_no_medicosocial' => '(age BETWEEN 18 AND 65) AND JSON_LENGTH(properties) = 0',
-                                    'adult_with_cronic_illness' => '(age BETWEEN 18 AND 65) AND (JSON_CONTAINS(properties, \'"VSG_01"\') OR JSON_CONTAINS(properties, \'"VSG_BPO"\') OR JSON_CONTAINS(properties, \'"VSG_HEP"\') OR JSON_CONTAINS(properties, \'"VSG_IR"\'))',
-                                    'adult_with_disabilities' => '(age BETWEEN 18 AND 65) AND (JSON_CONTAINS(properties, \'"VDH_01"\') OR JSON_CONTAINS(properties, \'"VDH_02"\'))',
-                                    'adult_without_family' => '(age BETWEEN 18 AND 65) AND JSON_CONTAINS(properties, \'"VFA_01"\')',
-                                    'familiy_with_domestic_violence_case' => 'JSON_CONTAINS(properties, \'"VFV_03"\')',
-                                    'woman_fertile_age' => '(age BETWEEN 18 AND 45) AND gender = "female"',
-                                    'woman_postpartum' => 'JSON_CONTAINS(properties, \'"VGR_02"\') or JSON_CONTAINS(properties, \'"VGR_08"\')',
-                                    'underage_mother' => '(age < 18) AND JSON_CONTAINS(properties, \'"VGR_03"\')',
-                                    'family_planning' => null,
-                                    'person_without_gp' => 'JSON_CONTAINS(properties, \'"VSA_02"\')',
-                                    'elderly' => '(age >= 65)',
-                                    'elderly_without_family' => '(age >= 65) AND JSON_CONTAINS(properties, \'"VFA_01"\')',
-                                    'elderly_with_cronic_illness' => '(age >= 65) AND (JSON_CONTAINS(properties, \'"VSG_01"\') OR JSON_CONTAINS(properties, \'"VSG_BPO"\') OR JSON_CONTAINS(properties, \'"VSG_HEP"\') OR JSON_CONTAINS(properties, \'"VSG_IR"\'))',
-                                    'elderly_with_disabilities' => '(age >= 65) AND (JSON_CONTAINS(properties, \'"VDH_01"\') OR JSON_CONTAINS(properties, \'"VDH_02"\'))',
-                                }])
-                            ->filter()
-                            ->map(fn (string $filter, string $indicator) => "WHEN ({$filter}) THEN \"{$indicator}\"")
-                            ->implode('');
+                ->fromSub(function (QueryBuilder $query) use ($indicators) {
+                    $cases = $indicators
+                        ->mapWithKeys(fn (string $indicator) => [
+                            $indicator => match ($indicator) {
+                                'adult_no_medicosocial' => '(age BETWEEN 18 AND 65) AND JSON_LENGTH(properties) = 0',
+                                'adult_with_cronic_illness' => '(age BETWEEN 18 AND 65) AND (JSON_CONTAINS(properties, \'"VSG_01"\') OR JSON_CONTAINS(properties, \'"VSG_BPO"\') OR JSON_CONTAINS(properties, \'"VSG_HEP"\') OR JSON_CONTAINS(properties, \'"VSG_IR"\'))',
+                                'adult_with_disabilities' => '(age BETWEEN 18 AND 65) AND (JSON_CONTAINS(properties, \'"VDH_01"\') OR JSON_CONTAINS(properties, \'"VDH_02"\'))',
+                                'adult_without_family' => '(age BETWEEN 18 AND 65) AND JSON_CONTAINS(properties, \'"VFA_01"\')',
+                                'familiy_with_domestic_violence_case' => 'JSON_CONTAINS(properties, \'"VFV_03"\')',
+                                'woman_fertile_age' => '(age BETWEEN 18 AND 45) AND gender = "female"',
+                                'woman_postpartum' => 'JSON_CONTAINS(properties, \'"VGR_02"\') or JSON_CONTAINS(properties, \'"VGR_08"\')',
+                                'underage_mother' => '(age < 18) AND JSON_CONTAINS(properties, \'"VGR_03"\')',
+                                'family_planning' => null, // TODO: join services table
+                                'person_without_gp' => 'JSON_CONTAINS(properties, \'"VSA_02"\')',
+                                'elderly' => '(age >= 65)',
+                                'elderly_without_family' => '(age >= 65) AND JSON_CONTAINS(properties, \'"VFA_01"\')',
+                                'elderly_with_cronic_illness' => '(age >= 65) AND (JSON_CONTAINS(properties, \'"VSG_01"\') OR JSON_CONTAINS(properties, \'"VSG_BPO"\') OR JSON_CONTAINS(properties, \'"VSG_HEP"\') OR JSON_CONTAINS(properties, \'"VSG_IR"\'))',
+                                'elderly_with_disabilities' => '(age >= 65) AND (JSON_CONTAINS(properties, \'"VDH_01"\') OR JSON_CONTAINS(properties, \'"VDH_02"\'))',
+                            }])
+                        ->filter()
+                        ->map(fn (string $filter, string $indicator) => "WHEN ({$filter}) THEN \"{$indicator}\"")
+                        ->implode(' ');
 
-                        $query->selectRaw(new Alias(new Expression("CASE {$cases} END"), 'indicator'))
-                            ->groupBy('indicator');
-                    },
-                    function (Builder $query) {
-                        $query->addSelect(new Alias(new Value('total'), 'indicator'));
-                    }
-                ),
+                    $query
+                        ->select(['*'])
+                        ->selectRaw(new Alias(new Expression("CASE {$cases} END"), 'indicator'))
+                        ->fromSub(function (QueryBuilder $query) {
+                            $query
+                                ->select(['activity_log.*', 'beneficiaries.nurse_id', 'beneficiaries.gender'])
+                                ->selectRaw('TIMESTAMPDIFF(YEAR, date_of_birth, ?) AS age', [$this->report->date_from->toDateString()])
+                                ->from('activity_log')
+                                ->leftJoin('beneficiaries', function (JoinClause $join) {
+                                    $join->on('beneficiaries.id', '=', 'activity_log.subject_id')
+                                        ->where('activity_log.subject_type', 'beneficiary');
+                                });
+                        }, 'activity_log');
+                }, 'activity_log')
+                ->addSelect('indicator')
+                ->whereNotNull('indicator')
+                ->groupBy('indicator'),
             'indicator'
         );
     }
