5e5381c6135f8b1a141740f00bb34ea4354a78e2
Andrei Ioniță <hi@andrei.io>
2023-07-14 08:38:24 +0100

    refactor custom filament routes

--- routes/filament.php
+++ routes/filament.php
@@ -4,10 +4,4 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain(config('filament.domain'))
-    ->name('filament.')
-    ->group(function () {
-        Route::prefix(config('filament.path'))->group(function () {
-            Route::get('/welcome/{user}', \App\Http\Livewire\Welcome::class)->name('auth.welcome');
-        });
-    });
+Route::get('/welcome/{user}', \App\Http\Livewire\Welcome::class)->name('auth.welcome');
