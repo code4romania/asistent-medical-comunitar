a646b116443f22648c6cf403303627b75b9768d8
Andrei Ioniță <hi@andrei.io>
2024-01-25 12:12:20 +0100

    cleanup

--- app/Filament/Resources/InterventionResource/Pages/ManageInterventions.php
+++ app/Filament/Resources/InterventionResource/Pages/ManageInterventions.php
@@ -5,17 +5,9 @@ declare(strict_types=1);
 namespace App\Filament\Resources\InterventionResource\Pages;
 
 use App\Filament\Resources\InterventionResource;
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageInterventions extends ManageRecords
 {
     protected static string $resource = InterventionResource::class;
-
-    protected function getActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
