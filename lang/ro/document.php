695272b73f93a40437936f90bebd199e6053fc3d
Andrei Ioniță <hi@andrei.io>
2024-01-31 17:04:47 +0000

    feat: add document preview

--- lang/ro/document.php
+++ lang/ro/document.php
@@ -14,13 +14,21 @@ return [
         'edit' => 'Editează document',
         'update' => 'Actualizează document',
         'delete' => 'Șterge document',
+        'download' => 'Descarcă',
     ],
 
     'summary' => 'Detalii document',
+    'preview' => 'Vizualizare document',
 
     'empty' => [
         'title' => 'Nu există documente încărcate',
         'create' => 'Încarcă documente',
         'description' => 'Încarcă aici toate documentele de identificare ale beneficiarului de care ai nevoie pentru evidența lui.',
     ],
+
+    'empty_preview' => [
+        'title' => 'Formatul documentului nu permite previzualizarea',
+        'description' => 'Descărcați documentul pentru a-l putea vizualiza',
+    ],
+
 ];
