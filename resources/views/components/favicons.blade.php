ce05ebc24a4be12c55009e7c3aebf1bfcf001a68
Andrei Ioniță <hi@andrei.io>
2024-03-01 16:03:16 +0000

    fix: images not loading

--- resources/views/components/favicons.blade.php
+++ resources/views/components/favicons.blade.php
@@ -1,5 +1,5 @@
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/assets/apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/assets/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/favicon-16x16.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/images/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/images/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/images/favicon-16x16.png') }}">
 <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
 <meta name="theme-color" content="#FFFFFF">
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('assets/logo.png') }}" class="logo" alt="{{ $slot }}">
+<img src="{{ asset('/images/logo.png') }}" class="logo" alt="{{ $slot }}">
 </a>
 </td>
 </tr>
