eea4c18ebd7c74818e5ded19f915572536812e45
Andrei Ioniță <hi@andrei.io>
2024-03-06 12:31:08 +0000

    squash import

--- database/migrations/2014_10_11_000010_create_icd10am_tables.php
+++ database/migrations/2014_10_11_000010_create_icd10am_tables.php
@@ -2,15 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Imports\ICD10AM\ICD10AMClassImport;
-use App\Imports\ICD10AM\ICD10AMDiagnosticsImport;
-use App\Imports\ICD10AM\ICD10AMSubclassImport;
 use App\Models\ICD10AM\ICD10AMClass;
 use App\Models\ICD10AM\ICD10AMSubclass;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
-use Maatwebsite\Excel\Facades\Excel;
 
 return new class extends Migration
 {
@@ -36,8 +34,8 @@ return new class extends Migration
             $table->string('name');
         });
 
-        Excel::import(new ICD10AMClassImport, database_path('data/icd10am/classes.csv'));
-        Excel::import(new ICD10AMSubclassImport, database_path('data/icd10am/subclasses.csv'));
-        Excel::import(new ICD10AMDiagnosticsImport, database_path('data/icd10am/diagnostics.csv'));
+        DB::unprepared(
+            File::get(database_path('data/icd10am.sql'))
+        );
     }
 };
