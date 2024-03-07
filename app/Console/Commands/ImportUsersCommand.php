8b0a3ecb530cff20b4ed39c4e73d936f4fac10c7
Andrei Ioniță <hi@andrei.io>
2024-03-07 10:50:36 +0000

    feat: add user importer

---
+++ app/Console/Commands/ImportUsersCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Enums\User\Role;
+use App\Imports\AdminsImport;
+use App\Imports\CoordinatorsImport;
+use App\Imports\NursesImport;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ImportUsersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:import-users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run user importer.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $role = $this->choice('What role should we assign to the imported users?', Role::options());
+
+        $files = collect(Storage::disk('local')->files('import'))
+            ->filter(fn (string $file) => \in_array(pathinfo(Str::lower($file), \PATHINFO_EXTENSION), ['csv', 'xlsx']))
+            ->values()
+            ->all();
+
+        $file = $this->choice('What file should we import?', $files);
+
+        $importer = match (Role::tryFrom($role)) {
+            Role::ADMIN => AdminsImport::class,
+            Role::COORDINATOR => CoordinatorsImport::class,
+            // Role::NURSE => NursesImport::class,
+        };
+
+        try {
+            (new $importer)->import($file, 'local');
+
+            Storage::disk('local')->delete($file);
+        } catch (\Throwable $th) {
+            $this->error($th->getMessage());
+
+            return self::FAILURE;
+        }
+
+        return self::SUCCESS;
+    }
+}
--- app/Imports/AdminsImport.php
+++ app/Imports/AdminsImport.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace App\Imports;
 
 use App\Enums\User\Role;
-use App\Models\County;
 use App\Models\User;
 use App\Services\Sanitize;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Concerns\ToModel;
@@ -17,27 +15,17 @@ use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
 
-class CoordinatorsImport implements ToModel, ShouldQueue, WithChunkReading, WithValidation, SkipsEmptyRows, WithHeadingRow
+class AdminsImport implements ToModel, ShouldQueue, WithChunkReading, WithValidation, SkipsEmptyRows, WithHeadingRow
 {
     use Importable;
-
-    private array $counties;
-
-    public function __construct()
-    {
-        $this->counties = County::all()
-            ->pluck('name', 'id')
-            ->toArray();
-    }
 
     public function model(array $row): User
     {
         return new User([
-            'role' => Role::COORDINATOR,
+            'role' => Role::ADMIN,
             'first_name' => $row['prenume'],
             'last_name' => $row['nume'],
             'email' => $row['e_mail'],
-            'county_id' => $this->getCountyId($row['judet']),
         ]);
     }
 
@@ -47,7 +35,6 @@ class CoordinatorsImport implements ToModel, ShouldQueue, WithChunkReading, With
             'prenume' => ['required', 'string'],
             'nume' => ['required', 'string'],
             'e_mail' => ['required', 'email', 'unique:users,email'],
-            'judet' => ['required', 'string'],
         ];
     }
 
@@ -56,7 +43,6 @@ class CoordinatorsImport implements ToModel, ShouldQueue, WithChunkReading, With
         $row['prenume'] = Sanitize::title($row['prenume']);
         $row['nume'] = Sanitize::title($row['nume']);
         $row['e_mail'] = Sanitize::email($row['e_mail']);
-        $row['judet'] = Sanitize::text($row['judet']);
 
         return $row;
     }
@@ -69,13 +55,5 @@ class CoordinatorsImport implements ToModel, ShouldQueue, WithChunkReading, With
     public function chunkSize(): int
     {
         return 10;
-    }
-
-    private function getCountyId(string $rawCountyName): ?int
-    {
-        $countyId = collect($this->counties)
-            ->search(fn (string $county, int $id) => Str::slug($county) === Str::slug($rawCountyName));
-
-        return $countyId ?: null;
     }
 }
