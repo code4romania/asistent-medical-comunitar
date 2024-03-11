7be8b96ad2149134a1f76730f2d2bc9b1555cf29
Andrei Ioniță <hi@andrei.io>
2024-03-11 11:34:53 +0000

    feat: add invitation resend action (#185)

--- lang/ro/user.php
+++ lang/ro/user.php
@@ -46,6 +46,7 @@ return [
         'invite' => 'Invită utilizator',
         'activate' => 'Activează cont utilizator',
         'deactivate' => 'Dezactivează cont utilizator',
+        'resend_invitation' => 'Retrimite invitația',
     ],
 
     'action_activate_confirm' => [
@@ -60,6 +61,15 @@ return [
         'text' => 'Ești sigur că vrei să dezactivezi acest utilizator?',
         'action' => 'Dezactivează',
         'success' => 'Utilizatorul a fost dezactivat cu succes.',
+    ],
+
+    'action_resend_invitation_confirm' => [
+        'title' => 'Retrimite invitația pe email',
+        'text' => 'Ești sigur că vrei să retrimiți invitația acest utilizator? Poți retrimite invitația doar o dată pe oră.',
+        'action' => 'Trimite',
+        'success' => 'Invitația a fost trimisă cu succes.',
+        'failure_title' => 'Invitația nu a putut fi trimisă.',
+        'failure_body' => 'Acestui utilizator i-a fost retrimisă invitația recent. Te rugăm să mai încerci peste o oră.',
     ],
 
 ];
