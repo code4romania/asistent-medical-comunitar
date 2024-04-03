036c12ed4997c48f0f6a830f2ac0fdd75ecc3982
Andrei Ioniță <hi@andrei.io>
2024-04-03 14:55:27 +0100

    simplify

--- app/Http/Middleware/FixMissingPasswordSetAtAttribute.php
+++ app/Http/Middleware/FixMissingPasswordSetAtAttribute.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class FixMissingPasswordSetAtAttribute
@@ -19,32 +18,14 @@ class FixMissingPasswordSetAtAttribute
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Can't do anything if we're not using the database session driver.
-        if (config('session.driver') !== 'database') {
-            return $next($request);
-        }
-
-        // Don't need to do anything if attribute is already set.
-        if ($request->user()->hasSetPassword()) {
-            return $next($request);
-        }
-
-        // If the user has recent sessions, then we can safely assume they have
+        // If the user got here, it's pretty clear they have
         // set their password, so we'll mark it as such.
-        if ($this->hasSessions($request->user())) {
-            $request->user()
+        if (! Auth::user()->hasSetPassword()) {
+            Auth::user()
                 ->markPasswordAsSet()
                 ->save();
         }
 
         return $next($request);
-    }
-
-    private function hasSessions(User $user): bool
-    {
-        return DB::connection(config('session.connection'))
-            ->table(config('session.table', 'sessions'))
-            ->where('user_id', $user->getAuthIdentifier())
-            ->exists();
     }
 }
