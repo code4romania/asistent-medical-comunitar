787043190312c9b62dba33ca0662d194ab970382
Andrei Ioniță <hi@andrei.io>
2024-04-03 12:43:51 +0100

    fix: move auth events to the activitylog

--- app/Listeners/Auth/Failed.php
+++ app/Listeners/Auth/Failed.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Listeners\Auth;
 
 use Illuminate\Auth\Events\Failed as Event;
-use Illuminate\Support\Facades\Log;
 
 class Failed
 {
     public function handle(Event $event): void
     {
-        Log::info("[Failed] User {$event->credentials['email']} login failed");
+        activity('auth')
+            ->performedOn($event->user)
+            ->event('failed')
+            ->log('failed');
     }
 }
--- app/Listeners/Auth/Login.php
+++ app/Listeners/Auth/Login.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Listeners\Auth;
 
 use Illuminate\Auth\Events\Login as Event;
-use Illuminate\Support\Facades\Log;
 
 class Login
 {
     public function handle(Event $event): void
     {
-        Log::info("[Login] User {$event->user->email} logged in");
+        activity('auth')
+            ->performedOn($event->user)
+            ->event('login')
+            ->log('login');
     }
 }
