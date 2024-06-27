a44c13b28ff5ddf521d02a80bb52c31ffd3faf60
Andrei Ioniță <hi@andrei.io>
2024-06-27 17:00:52 +0300

    fix: intervention return type

--- app/Concerns/MorphsIntervention.php
+++ app/Concerns/MorphsIntervention.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Concerns;
 
 use App\Models\Intervention;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait MorphsIntervention
 {
-    public function intervention()
+    public function intervention(): MorphOne
     {
         return $this->morphOne(Intervention::class, 'interventionable');
     }
