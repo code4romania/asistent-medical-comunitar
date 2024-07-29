e383146138a2f5ffd1e44e9d1597b0339ae32463
Andrei Ioniță <hi@andrei.io>
2024-07-29 12:12:42 +0100

    feat: custom error pages

---
+++ lang/en/error.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    401 => [
+        'title' => 'Unauthorized',
+        'message' => 'Sorry, you are not authorized to access this page.',
+    ],
+    403 => [
+        'title' => 'Forbidden',
+        'message' => 'Sorry, you are forbidden from accessing this page.',
+    ],
+    404 => [
+        'title' => 'Not found',
+        'message' => 'Sorry, the page you are looking for could not be found.',
+    ],
+    419 => [
+        'title' => 'Expired session',
+        'message' => 'Sorry, your session has expired. Please refresh and try again.',
+    ],
+    429 => [
+        'title' => 'Too Many Requests',
+        'message' => 'Sorry, you are making too many requests to our servers.',
+    ],
+    500 => [
+        'title' => 'Internal Server Error',
+        'message' => 'Sorry, something went wrong on our servers.',
+    ],
+    503 => [
+        'title' => 'Service Unavailable',
+        'message' => 'Sorry, we are doing some maintenance. Please check back soon.',
+    ],
+
+];
