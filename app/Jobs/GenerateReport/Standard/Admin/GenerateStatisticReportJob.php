77b3d25f42ad19bd01f9f8315926153209451f46
Andrei Ioniță <hi@andrei.io>
2024-10-10 15:50:29 +0100

    feat: generate admin report job

--- app/Jobs/GenerateReport/Standard/Admin/GenerateStatisticReportJob.php
+++ app/Jobs/GenerateReport/Standard/Admin/GenerateStatisticReportJob.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Jobs\GenerateReport\Standard\Admin;
 
+use App\Contracts\Enums\HasQuery;
 use App\Jobs\GenerateReport\Standard\GenerateStandardReportJob;
+use App\Models\County;
 use App\Models\Report;
-use Exception;
 use Illuminate\Support\Collection;
+use Tpetry\QueryExpressions\Function\Aggregate\Count;
+use Tpetry\QueryExpressions\Language\Alias;
 
 class GenerateStatisticReportJob extends GenerateStandardReportJob
 {
@@ -22,6 +25,47 @@ class GenerateStatisticReportJob extends GenerateStandardReportJob
 
     public function generate(): void
     {
-        throw new Exception('Not implemented');
+        $counties = County::query()
+            ->whereIn('id', $this->counties)
+            ->get();
+
+        $this->report->data = [
+            [
+                'title' => $this->getCategory()->label(),
+
+                'columns' => $counties
+                    ->map(fn (County $county) => [
+                        'name' => "county-{$county->id}",
+                        'label' => $county->name,
+                    ])
+                    ->values(),
+
+                'data' => $this->report->indicators()
+                    ->mapWithKeys(function (HasQuery $indicator) use ($counties) {
+                        /** @var ReportQuery $reportQuery */
+                        $reportQuery = $indicator->class();
+
+                        $results = $reportQuery::build($this->report)
+                            ->select([
+                                'county_id' => County::query()
+                                    ->select('counties.id')
+                                    ->join('users', 'users.activity_county_id', 'counties.id')
+                                    ->whereColumn('users.id', 'nurse_id')
+                                    ->take(1),
+                                new Alias(new Count('*'), 'count'),
+                            ])
+                            ->groupBy('county_id')
+                            ->toBase()
+                            ->get()
+                            ->pluck('count', 'county_id');
+
+                        return [
+                            $indicator->label() => $counties->mapWithKeys(fn (County $county) => [
+                                "county-{$county->id}" => $results->get($county->id, 0),
+                            ]),
+                        ];
+                    }),
+            ],
+        ];
     }
 }
