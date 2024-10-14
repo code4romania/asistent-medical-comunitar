2adbc00217e61bbfdfaa15260a23727709f81802
Andrei Ioniță <hi@andrei.io>
2024-10-14 21:30:51 +0100

    fix: wrong query for "Gravidă neînscrisă la medicul de familie"

--- app/Reports/Queries/Pregnant/P08.php
+++ app/Reports/Queries/Pregnant/P08.php
@@ -11,13 +11,14 @@ use Illuminate\Database\Eloquent\Builder;
 class P08 extends ReportQuery
 {
     /**
-     * Sum beneficiari with Gravidă adultă (VGR_04); Neînscris la medic de familie (VSA_02).
+     * Sum beneficiari with Gravidă adultă (VGR_04) OR Minoră gravidă (VGR_01); Neînscris la medic de familie (VSA_02).
      */
     public static function query(): Builder
     {
         return Beneficiary::query()
             ->whereHasVulnerabilities(function (Builder $query) {
-                $query->whereJsonContains('properties', ['VGR_04', 'VSA_02']);
+                $query->whereJsonOverlaps('properties', ['VGR_04', 'VGR_01'])
+                    ->whereJsonContains('properties', 'VSA_02');
             });
     }
 }
