a18c026fed6688c5705f2bde4fa5ced17c1ae8e1
Andrei Ioniță <hi@andrei.io>
2024-10-14 08:22:27 +0100

    fix: wrong indicator for "Copil care nu primește vitamina D și Fier"

--- app/Reports/Queries/Child/C06.php
+++ app/Reports/Queries/Child/C06.php
@@ -11,13 +11,13 @@ use Illuminate\Database\Eloquent\Builder;
 class C06 extends ReportQuery
 {
     /**
-     * Sum beneficiari with Copil care nu primește vitamina D și Fier (VCS_03); Copil 0-1 ani (VCV_01) OR Copil 1-5 ani (VCV_02) OR Copil 5-14 ani (VCV_03).
+     * Sum beneficiari with Copil care nu primește vitamina D și Fier (VSC_03); Copil 0-1 ani (VCV_01) OR Copil 1-5 ani (VCV_02) OR Copil 5-14 ani (VCV_03).
      */
     public static function query(): Builder
     {
         return Beneficiary::query()
             ->whereHasVulnerabilities(function (Builder $query) {
-                $query->whereJsonContains('properties', 'VCS_03')
+                $query->whereJsonContains('properties', 'VSC_03')
                     ->whereJsonOverlaps('properties', ['VCV_01', 'VCV_02', 'VCV_03']);
             });
     }
