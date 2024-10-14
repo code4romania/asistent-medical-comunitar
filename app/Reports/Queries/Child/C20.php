db759e68df5f01386db477238f46154005ff5ea7
Andrei Ioniță <hi@andrei.io>
2024-10-14 08:52:08 +0100

    fix: wrong indicator for "Vaccinat conform calendarului"

--- app/Reports/Queries/Child/C20.php
+++ app/Reports/Queries/Child/C20.php
@@ -11,13 +11,13 @@ use Illuminate\Database\Eloquent\Builder;
 class C20 extends ReportQuery
 {
     /**
-     * Sum beneficiari with Vaccinat conform calendarului (VVC_99); Copil 0-1 ani (VCV_01) OR Copil 1-5 ani (VCV_02) OR Copil 5-14 ani (VCV_03).
+     * Sum beneficiari with Vaccinat conform calendarului (!= VVC_01); Copil 0-1 ani (VCV_01) OR Copil 1-5 ani (VCV_02) OR Copil 5-14 ani (VCV_03).
      */
     public static function query(): Builder
     {
         return Beneficiary::query()
             ->whereHasVulnerabilities(function (Builder $query) {
-                $query->whereJsonContains('properties', 'VVC_99')
+                $query->whereJsonDoesntContain('properties', 'VVC_01')
                     ->whereJsonOverlaps('properties', ['VCV_01', 'VCV_02', 'VCV_03']);
             });
     }
