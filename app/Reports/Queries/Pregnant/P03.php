c1a984989d0d22c531ddd5dcd23e866a9c5528fd
Andrei Ioniță <hi@andrei.io>
2024-10-15 20:05:25 +0100

    fix: wrong query for "Gravidă care a efectuat consultaţii prenatale"

--- app/Reports/Queries/Pregnant/P03.php
+++ app/Reports/Queries/Pregnant/P03.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Reports\Queries\Pregnant;
 
 use App\Models\Beneficiary;
+use App\Models\Catagraphy;
 use App\Reports\Queries\ReportQuery;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class P03 extends ReportQuery
 {
@@ -17,8 +19,10 @@ class P03 extends ReportQuery
     {
         return Beneficiary::query()
             ->whereHasVulnerabilities(function (Builder $query) {
-                $query->whereJsonOverlaps('properties', ['VGR_04', 'VGR_01'])
-                    ->whereJsonContains('properties', 'VGR_98');
+                $query->whereJsonOverlaps('properties', ['VGR_04', 'VGR_01']);
+            })
+            ->whereHasCatagraphyRelation(Catagraphy::class, function (QueryBuilder $query) {
+                $query->whereJsonContains('properties->attributes->cat_preg', 'VGR_98');
             });
     }
 }
