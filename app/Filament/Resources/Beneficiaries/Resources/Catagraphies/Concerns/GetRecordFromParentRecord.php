86aa1a42d93cf23b9507bd918a3696a563a3d9d0
Andrei Ioniță <hi@andrei.io>
2026-01-20 16:17:17 +0000

    fix: catagraphy parent record

--- app/Filament/Resources/Beneficiaries/Resources/Catagraphies/Concerns/GetRecordFromParentRecord.php
+++ app/Filament/Resources/Beneficiaries/Resources/Catagraphies/Concerns/GetRecordFromParentRecord.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Beneficiaries\Resources\Catagraphies\Concerns;
 
+use App\Models\Catagraphy;
+
 trait GetRecordFromParentRecord
 {
     public function mount(int | string | null $record = null): void
     {
-        $this->record = $this->getParentRecord()->catagraphy;
+        $this->record = $this->getRecord();
 
         $this->authorizeAccess();
 
         if (! method_exists($this, 'hasInfolist') || ! $this->hasInfolist()) {
             $this->fillForm();
         }
+    }
+
+    public function getRecord(): Catagraphy
+    {
+        return $this->getParentRecord()->catagraphy;
     }
 }
