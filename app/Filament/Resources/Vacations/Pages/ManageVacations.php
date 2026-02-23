768e1015942af131ccd676c68c24be861dfa6ab5
Andrei Ioniță <hi@andrei.io>
2026-02-23 14:04:54 +0000

    fix: vacations creation

--- app/Filament/Resources/Vacations/Pages/ManageVacations.php
+++ app/Filament/Resources/Vacations/Pages/ManageVacations.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filament\Resources\Vacations\Pages;
 
 use App\Filament\Resources\Vacations\VacationResource;
+use App\Models\Vacation;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ManageRecords;
 
@@ -15,7 +16,12 @@ class ManageVacations extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->using(function (array $data) {
+                    $data['nurse_id'] = auth()->id();
+
+                    return Vacation::create($data);
+                }),
         ];
     }
 }
