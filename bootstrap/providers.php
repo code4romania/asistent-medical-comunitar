28ef57a30951e63e8eea239a598163d9b92258df
Andrei Ioniță <hi@andrei.io>
2026-02-23 17:31:06 +0000

    fix: antivirus provider (#451)

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,4 +6,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FilamentServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    Aedart\Antivirus\Providers\AntivirusServiceProvider::class,
 ];
