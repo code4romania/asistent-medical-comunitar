386cc596014d25bcb4bdf84e5af811a592752362
Andrei Ioniță <hi@andrei.io>
2026-02-23 17:39:57 +0000

    fix: appointment creation (#452)

--- app/Filament/Resources/Appointments/Pages/CreateAppointment.php
+++ app/Filament/Resources/Appointments/Pages/CreateAppointment.php
@@ -6,8 +6,32 @@ namespace App\Filament\Resources\Appointments\Pages;
 
 use App\Filament\Resources\Appointments\AppointmentResource;
 use Filament\Resources\Pages\CreateRecord;
+use Livewire\Attributes\Url;
 
 class CreateAppointment extends CreateRecord
 {
     protected static string $resource = AppointmentResource::class;
+
+    #[Url]
+    public ?int $beneficiary = null;
+
+    protected static bool $canCreateAnother = false;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['nurse_id'] = auth()->id();
+
+        return $data;
+    }
+
+    protected function afterFill(): void
+    {
+        if (blank($this->beneficiary)) {
+            return;
+        }
+
+        $this->form->fill([
+            'beneficiary_id' => $this->beneficiary,
+        ]);
+    }
 }
